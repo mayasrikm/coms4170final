87e0acdfdd3aabeb7666b41ca5337acc8247d0e8
Maya Srikanth <ms6198@columbia.edu>
2024-04-15 12:41:00 -0400

    skeleton for slap rules

--- app.py
+++ app.py
@@ -22,5 +22,31 @@ def rules():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+@app.route('/standard-doubles')
+def standard_doubles():
+    return render_template('rules/standard_doubles.html')
+
+@app.route('/standard-sandwich')
+def standard_sandwich():
+    return render_template('rules/standard_sandwich.html')
+
+@app.route('/extra-top')
+def extra_top():
+    return render_template('rules/extra_top.html')
+
+@app.route('/extra-royal')
+def extra_royal():
+    return render_template('rules/extra_royal.html')
+
+@app.route('/extra-tens')
+def extra_tens():
+    return render_template('rules/extra_tens.html')
+
+@app.route('/extra-four')
+def extra_four():
+    return render_template('rules/extra_four.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
